4efbd3de127a2bfc9f1f3ccce782712aa9f1b759
Eric Brandwein <brandweineric@gmail.com>
2020-05-21 19:59:31 -0300

    Agrega resultados a ej2.sh

--- stackar/ej2.sh
+++ stackar/ej2.sh
@@ -3,3 +3,10 @@
 mvn clean install
 
 $BROWSER target/site/jacoco/index.html
+
+### Preguntas:
+# ¿Cuántas lı́neas cubiertas reporta JaCoCo?
+# ¿Cuántos branches cubiertos reporta JaCoCo?
+
+### Respuestas:
+# JaCoCo reportó 46 líneas cubiertas de 53, y 25 de 26 branches cubiertos.
