a589c4b4c8a620f63990c722ddb8516eea877c06
Eric Brandwein <brandweineric@gmail.com>
2020-05-21 20:37:46 -0300

    Extrae GENERATION_TIME en ej1.sh

--- stackar/ej1.sh
+++ stackar/ej1.sh
@@ -5,12 +5,13 @@ BUILD_DIR=build
 SRC_DIR=src
 RANDOOP_CLASS_PATH=lib/randoop-all-4.2.3.jar
 TEST_GENERATION_CLASSPATH=$RANDOOP_CLASS_PATH:$BUILD_DIR
+GENERATION_TIME=15
 
 mkdir -p build
 javac -classpath lib/randoop-all-4.2.3.jar -d $BUILD_DIR $SRC_DIR/${STACKAR_DIR}/StackAr.java
 
 java -ea -classpath $TEST_GENERATION_CLASSPATH randoop.main.Main gentests \
-    --testclass=org.autotest.StackAr --time-limit=15 --testsperfile=500 \
+    --testclass=org.autotest.StackAr --time-limit=$GENERATION_TIME --testsperfile=500 \
     --junit-output-dir=src/test/java
 
 ### Preguntas:
