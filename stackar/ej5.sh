98f7318e4a58a1ce2313dd4bbb43ce0cb1349a79
Eric Brandwein <brandweineric@gmail.com>
2020-05-21 22:02:44 -0300

    Abre resultado de ej5 en el browser

--- stackar/ej5.sh
+++ stackar/ej5.sh
@@ -2,7 +2,7 @@
 
 mvn -e clean install org.pitest:pitest-maven:mutationCoverage
 
-# Los resultados están en target/pit-reports
+$BROWSER target/pit-reports/*/index.html
 
 ### Preguntas:
 # 1. ¿Cuántas mutantes construye PiTest? ¿Cuál es el mutation score
