c57b1f67edab08a010ef1a22a5563f1b207bee80
Eric Brandwein <brandweineric@gmail.com>
2020-05-21 23:00:53 -0300

    Respuesta de pregunta 2 de ejercicio 5

--- stackar/ej5.sh
+++ stackar/ej5.sh
@@ -13,4 +13,11 @@ $BROWSER target/pit-reports/*/index.html
 
 ### Respuestas:
 # 1. PiTest construyó 52 mutantes, y su mutation score fue de 39/52 (75%).
+# 2. El mejor mutation score que pude obtener fue de 46/52 (88%). Los mutantes
+# que modificaban repOk no se pueden matar por el hecho de que no se puede armar
+# una instancia de la clase que haga que repOk devuelva false, o sea que cualquier
+# mutante que relaje la condición del repOk no podrá ser matado. Además, PiTest encontró
+# un mutante equivalente, que modificaba una multiplicación por una división
+# en hashCode(). Era una multiplicación por 1, cuyo resultado será siempre igual a la
+# división por 1.
 
